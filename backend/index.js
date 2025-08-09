5c289278ce933ed556e010d094ec870126ba0fa8
jeyaraman-s <jeyaraman@frigate.ai>
2025-08-09 10:45:03 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const app = express();
 const canvasRoutes = require('./routes/canvasRoutes');
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://canvas-project-silk.vercel.app';
 
 // Use cors middleware
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: [FRONTEND_URL, 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
